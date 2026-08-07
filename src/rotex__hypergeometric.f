ef287c34ccea0f467702ad269758700191901a9f
banana-bred <j.forer@posteo.net>
2026-08-07 17:10:57 -0400

    added special case Appell F2 for quadrupoles

--- src/rotex__hypergeometric.f
+++ src/rotex__hypergeometric.f
@@ -1,14 +1,16 @@
 ! ================================================================================================================================ !
 module rotex__hypergeometric
-  !! For calculating the Gauss hypergeometric function ₂F₁(a,b;c;z)
+  !! For calculating the Gauss hypergeometric function ₂F₁(a,b;c;z) and Appel hypergeometric function F₂(a,b₁,b₂;c₁,c₂,x,y)
   use rotex__kinds,     only: dp, qp
   use rotex__constants, only: macheps_dp
+  use rotex__system,    only: stderr, die
 
   implicit none (type, external)
 
   private
 
   public :: f21
+  public :: appell_f2_xy_ac1
 
   interface f21
     !! Interface for calculating the Gauss hypergeometric function ₂F₁(a,b;c;z)
@@ -24,7 +26,7 @@ contains
 ! ================================================================================================================================ !
 
   ! ------------------------------------------------------------------------------------------------------------------------------ !
-  impure elemental module function f21_r(a, b, c, x) result(res)
+  impure elemental function f21_r(a, b, c, x) result(res)
     !! Returns one of the following transforms for real a, b, c, x. These transformations are not valid for
     !! integral b-a or c-a-b
     !!   1. ₂F₁(a,b;c;x) = (1-x)^{-b} ₂F₁(b,c-a;c;x/(x-1))
@@ -119,7 +121,7 @@ contains
   end function f21_r
 
   ! ------------------------------------------------------------------------------------------------------------------------------ !
-  impure elemental module function f21_c(a, b, c, x) result(res)
+  impure elemental function f21_c(a, b, c, x) result(res)
     !! Returns one of the following transforms for complex a, b, c; real x
     !!   1. ₂F₁(a,b;c;x) = (1-x)^{-b} ₂F₁(b,c-a;c;x/(x-1))
     !!   2. ₂F₁(a,b;c;x) = (1-x)^{-a} ₂F₁(a,c-b;c;x/(x-1))
@@ -205,7 +207,6 @@ contains
 
     use rotex__utils,     only: isin
     use rotex__globals,   only: ABC_THRESHOLD => HYPGEO_ABC_THRESHOLD
-    use rotex__system,    only: die, stderr
 
     implicit none (type, external)
 
@@ -641,241 +642,78 @@ contains
 
   end function olver_2f1_dlmf_15_8_12
 
-  ! ! ------------------------------------------------------------------------------------------------------------------------------ !
-  ! pure subroutine f21_ode_rhs(x, y, a, b, c, dydx)
-  !   !! y(1)    = y  ; y(2)    = y'
-  !   !! dydx(1) = y' ; dydx(2) = y''
-  !   use rotex__system, only: die
-  !   implicit none (type, external)
-  !   real(dp),    intent(in)  :: x
-  !   complex(dp), intent(in)  :: y(2)
-  !   complex(dp), intent(in)  :: a, b, c
-  !   complex(dp), intent(out) :: dydx(2)
-  !   complex(dp) :: denom
-  !   denom = cmplx(x*(1.0_dp - x), 0.0_dp, kind=dp)
-  !   if(abs(denom) .eq. 0) call die("x = 0 or 1 detected // illegal value")
-  !   dydx(1) = y(2)
-  !   dydx(2) = ( -(c - (a+b+1.0_dp)*x)*y(2) + a*b*y(1) ) / denom
-  ! end subroutine f21_ode_rhs
+  ! ------------------------------------------------------------------------------------------------------------------------------ !
+  impure elemental function appell_f2_xy_ac1(a, b1, b2, c1, x) result(res)
+    !! Calculate the Appell hypergeometric function for equal arguments x and y, and
+    !! equal parameters a and c₂
+    !!
+    !!   F₂(a,b1,b2;c₁,c₂;x,y) = Σ_{m,n=0}^∞ (a)_{m+n} (b₁)_m (b₂)_n / ( (c₁)_m (c₁)_n m! n!) × x^m y^n
+    !!
+    !! with
+    !!
+    !!   a  = -λ + 1 + iξ
+    !!   b1 =  l + λ + 1 - iη_i
+    !!   b2 =  l + 1 + iη_f
+    !!   c1 =  λ + 1 + iξ
+    !!   c2 = -λ + 1 + iξ
+    !!
+    !! The equal arguments collapse and parameters collapse to a single sum over n
+    !! modulated by the Gausse hypergeometric function
+    !!
+    !!   F₂ = Σ_n (b₂)_n / n! * x^n * ₂F₁(a+n, b₁; c₁; x)
 
-  ! ! ------------------------------------------------------------------------------------------------------------------------------ !
-  ! pure subroutine get_hypgeo_ode_matrix(x, a, b, c, M)
-  !   !! Set up the 2 x 2 ODE matrix
-  !   !!  |       0     ;      1                 |
-  !   !!  | ab/[x(1-x)] ;-[c-(a+b+1)x]/[x(1-x)] |
-  !   !! Solve the 2x2 matrix equation M
-  !   use rotex__system, only: die
-  !   implicit none (type, external)
-  !   real(dp),    intent(in)  :: x
-  !   complex(dp), intent(in)  :: a, b, c
-  !   complex(dp), intent(out) :: M(2,2)
-  !   complex(dp) :: denom
-  !   denom = cmplx(x*(1.0_dp - x), 0.0_dp, kind=dp)
-  !   if(abs(denom) .eq. 0) call die("x = 0 or 1 detected // illegal value")
-  !   M(1,1) = 0.0_dp
-  !   M(2,1) = (a*b)/denom
-  !   M(1,2) = (1.0_dp, 0.0_dp)
-  !   M(2,2) = -( c - (a+b+1.0_dp)*x ) / denom
-  ! end subroutine get_hypgeo_ode_matrix
+    implicit none(type, external)
 
-  ! ! ------------------------------------------------------------------------------------------------------------------------------ !
-  ! pure subroutine solve2x2(m, b, x)
-  !   !! Solve the 2x2 matrix equation Mx = b
-  !   implicit none (type, external)
-  !   complex(dp), intent(in)  :: m(2,2), b(2)
-  !   complex(dp), intent(out) :: x(2)
-  !   complex(dp) :: det
-  !   det  = m(1,1)*m(2,2) - m(1,2)*m(2,1)
-  !   x(1) = (  m(2,2)*b(1) - m(1,2)*b(2) ) / det
-  !   x(2) = ( -m(2,1)*b(1) + m(1,1)*b(2) ) / det
-  ! end subroutine solve2x2
+    complex(dp), intent(in) :: a, b1, b2, c1
+    real(dp), intent(in) :: x
+    complex(dp) :: res
 
-  ! ! ------------------------------------------------------------------------------------------------------------------------------ !
-  ! pure subroutine step_midpt(xn, h, a, b, c, Yn, Ynp1)
-  !   !! Implicit midpoint
-  !   !!   (I - (h/2)*Amid)Ynp1 = (I + (h/2)*Amid )Yn; Amid = A(xn + h/2)
-  !   implicit none (type, external)
-  !   real(dp), intent(in) :: xn
-  !     !! The point x(n)
-  !   real(dp), intent(in) :: h
-  !     !! Step size
-  !   complex(dp), intent(in) :: a, b, c
-  !     !! Hypergeometric function parameters
-  !   complex(dp), intent(in) :: Yn(2)
-  !     !! Solution vector y(n)
-  !   complex(dp), intent(out) :: Ynp1(2)
-  !     !! Solution vector y(n+1) at the next step
-  !   complex(dp) :: Amid(2,2), I(2,2), M1(2,2), M2(2,2), rhs(2)
-  !   real(dp) :: xmid
-  !   complex(dp), parameter :: one  = (1.0_dp, 0.0_dp)
-  !   complex(dp), parameter :: zero = (0.0_dp, 0.0_dp)
-  !   xmid = xn + 0.5_dp*h
-  !   call get_hypgeo_ode_matrix(xmid, a, b, c, Amid)
-  !   I = reshape([one, zero, zero, one], [2,2])
-  !   M1 = I - 0.5_dp*h*Amid
-  !   M2 = I + 0.5_dp*h*Amid
-  !   rhs = matmul(M2, Yn)
-  !   call solve2x2(M1, rhs, Ynp1)
-  ! end subroutine step_midpt
+    integer,  parameter :: NCONSEC = 3          !! consecutive small terms to declare convergence
+    integer,  parameter :: NMAX    = 1000       !! max number of loop iterations
+    real(qp), parameter :: EPS_REL = epsilon(1.0_dp) / 100.0_dp
+    real(qp), parameter :: MAXLOST = 5.0_qp
+      !! max number of digits that we will tolerate losing after cancellation
 
-  ! ! ------------------------------------------------------------------------------------------------------------------------------ !
-  ! pure subroutine f21_seed_series(a, b, c, x0, f0, fp0, tol)
-  !   !! Seed the ₂F₁ hypergeometric series starting from x0
-  !   use rotex__utils,     only: kbn_sum
-  !   use rotex__system,    only: die
-  !   use rotex__constants, only: macheps_dp
-  !   implicit none (type, external)
-  !   complex(dp), intent(in) :: a, b, c
-  !     !! Hypergeometric function parameters
-  !   real(dp), intent(in) :: x0
-  !     !! The starting point
-  !   complex(dp), intent(out) :: f0, fp0
-  !     !! Initial values for ₂F₁(a,b,c,x0) and its derivative
-  !   real(dp), intent(in), optional :: tol
-  !   integer,     parameter :: NMAX = 20000
-  !   real(dp),    parameter :: MINMAX = 1e-300_dp
-  !   complex(dp), parameter :: ONE  = (1.0_dp, 0.0_dp)
-  !   integer     :: n
-  !   real(dp)    :: tol_local
-  !   complex(dp) :: sumf, compf, sumfp, compfp, term, ratio
-  !   complex(dp) :: zn, z0
-  !   tol_local = 100*macheps_dp ; if(present(tol)) tol_local = tol
-  !   sumf = 1  ; compf = 0
-  !   sumfp = 0 ; compfp = 0
-  !   term = 1
-  !   if(x0 .le. 0) call die("Domain error: x0 must be > 0")
-  !   if(x0 .ge. 1) call die("Domain error: x0 must be < 1")
-  !   z0 = cmplx(x0, kind = dp)
-  !   do n=0, NMAX-1
-  !     zn = cmplx(n, kind = dp)
-  !     if (n .gt. 0) call kbn_sum(sumfp, compfp, zn*term/z0)
-  !     call kbn_sum(sumf, compf, term)
-  !     ratio = (a+zn)*(b+zn) / ((c+zn)*(n+ONE)) * z0
-  !     term = term * ratio
-  !     if(abs(term) .le. tol_local*max(MINMAX, abs(sumf))) exit
-  !   enddo
-  !   f0  = sumf  + compf
-  !   fp0 = sumfp + compfp
-  ! end subroutine f21_seed_series
+    real(qp) :: sumabs, lost, ratio
+    complex(qp) :: asum, term, pochb2, xx
+    complex(dp) :: g
+    integer :: n, nsmall
 
-  ! ! ------------------------------------------------------------------------------------------------------------------------------ !
-  ! impure elemental function f21_ode_eval(za, zb, zc, xtarg, rtol, atol, nsteps, nreject) result(res)
-  !   !! Evaluate the Hypergeometric function ₂F₁(za,zb;zc;xtarg) by stepping an initial solution of the
-  !   !! hypergeometric function towards xtarg. This routine should be called once the parameters
-  !   !! z{a,b,c} and argument xtarg have been transformed so that 0 < x < 1/2
+    if(abs(x) .ge. 0.5_dp) call die("Appell F2 is only defined for |x|+|y=x|<1 => 2|x| < 1")
 
-  !   use rotex__constants, only: macheps_dp, zero, one
-  !   use rotex__utils,     only: isin
-  !   use rotex__system,    only: die, stderr
+    asum   = 0
+    sumabs = 0
+    pochb2 = 1
+    xx   = 1
+    nsmall = 0
 
-  !   implicit none (type, external)
+    do n = 0, NMAX
 
-  !   complex(dp), intent(in) :: za, zb, zc
-  !     !! The parameters a,b,c
-  !   real(dp),    intent(in) :: xtarg
-  !     !! The argument x
-  !   real(dp),    intent(in), optional :: rtol, atol
-  !     !! The relative and absolute tolerance
-  !   integer,     intent(out), optional :: nsteps, nreject
-  !     !! The number of actual steps taken and trial steps rejected by the error test
-  !   complex(dp) :: res
+      g      = F21(a + n, b1, c1, x)
+      term   = pochb2 * xx * cmplx(g, kind=qp)
+      asum   = asum   + term
+      sumabs = sumabs + abs(term)
 
-  !   real(dp), parameter :: ATOL_DEFAULT_ = MACHEPS_DP * 100
-  !   real(dp), parameter :: RTOL_DEFAULT_ = MACHEPS_DP * 10000
-  !   real(dp), parameter :: X0MIN         = 1e-8_dp
-  !   real(dp), parameter :: X0MAX         = 1e-2_dp
-  !   real(dp), parameter :: X0SCALE       = 0.2_dp
-  !   real(dp), parameter :: HSCALE        = 0.1_dp
-  !   real(dp), parameter :: MIDPT_GUARD   = 0.2_dp
-  !   real(dp), parameter :: SAFE          = 0.9_dp
-  !   real(dp), parameter :: FACMIN        = .2_dp
-  !   real(dp), parameter :: FACMAX        = 5._dp
-  !   real(dp), parameter :: EXPERR        = -1._dp/(2._dp+1._dp)  ! -1/([p=2]+1)
+      if(abs(term) .lt. EPS_REL*max(abs(asum), tiny(1.0_qp))) then
+        nsmall = nsmall + 1
+        if(nsmall .ge. NCONSEC) exit
+      else
+        nsmall = 0
+      endif
 
-  !   integer  :: nsteps_, nreject_
-  !   real(dp) :: rtol_, atol_, err
-  !   real(dp) :: absab, A0, d0, h, x, x0, denom, hnew, sc1, sc2, xmin
-  !   complex(dp) :: zx
-  !   complex(dp) :: Y(2), Yfull(2), Yhalf(2), E(2)
+      pochb2 = pochb2 * (cmplx(b2, kind=qp) + n) / cmplx(n+1, kind=qp) ! (b₂)_{n+1}/(n+1)!
+      xx = xx * cmplx(x, kind=qp)
 
-  !   ! -- xtarg checks
-  !   if(isin(xtarg, X0MIN, ONE) .eqv. .false.) then
-  !     write(stderr, *)
-  !     write(stderr, '("XTARG: ", e20.10)') xtarg
-  !     write(stderr, '("X0MIN: ", e20.10)') X0MIN
-  !     call die("To solve the hypergeometric ODE, XTARG must be in the interval, [X0MIN,1).&
-  !       & X0MIN is chosen to be only slightly larger than zero because it is a pole of the ODE.")
-  !   endif
+      if(n .eq. NMAX) call die("Appel F2 did not converge within NMAX terms. Variable x is&
+        & is probably approaching 1/2 (channel threshold)")
 
-  !   ! -- defaults
-  !   rtol_ = RTOL_DEFAULT_ ; if(present(rtol)) rtol_ = rtol
-  !   atol_ = ATOL_DEFAULT_ ; if(present(atol)) atol_ = atol
-  !   if(present(nsteps))  nsteps_  = 0
-  !   if(present(nreject)) nreject_ = 0
+    enddo
 
-  !   absab = abs(za*zb)
+    res = cmplx(asum, kind=dp)
 
-  !   ! -- x0min ≤ x0 ≤ x0max
-  !   x0 = min(X0MAX, X0SCALE*abs(zc)/max(1._dp, abs(za*zb)))
-  !   x0 = max(X0MIN, x0)
+  end function appell_f2_xy_ac1
 
-  !   ! -- initialize stepsize h0
-  !   zx = cmplx(x0, kind = dp)
-  !   denom = x0 * (1.0_dp - x0)
-  !   A0 = max( absab, abs(zc - (za+zb+one)*zx) ) / denom
-  !   A0 = max( A0, one )
-  !   d0 = min( x0, one - x0 )
-  !   h  = min( HSCALE/A0, MIDPT_GUARD*d0, abs(xtarg - x0) )
 
-  !   ! -- x0 -> xtarg
-  !   x = x0
-  !   march: do
-
-  !     if(x + h .ge. xtarg) h = xtarg - x
-
-  !     call step_midpt(x,          h,        za, zb, zc, Y,     Yfull) ! <-- get full step
-  !     call step_midpt(x,          0.5_dp*h, za, zb, zc, Y,     Yhalf) ! <-- get half step
-  !     call step_midpt(x+0.5_dp*h, 0.5_dp*h, za, zb, zc, Yhalf, Yhalf) ! <-- get second half step
-
-  !     ! -- error estimate
-  !     E   = (Yhalf - Yfull) / 3.0_dp
-  !     sc1 = atol_ + rtol_*abs(Yhalf(1))
-  !     sc2 = atol_ + rtol_*abs(Yhalf(2))
-  !     err = max( abs(E(1))/sc1, abs(E(2))/sc2 )
-
-  !     if(err .le. one) then
-  !       ! -- OK
-  !       x = x + h
-  !       Y = Yhalf
-  !       nsteps_ = nsteps_ + 1
-
-  !       if(x .ge. xtarg) exit march
-
-  !       xmin = min(x, one-x)
-  !       hnew = h * SAFE * err**(EXPERR)
-  !       hnew = min( FACMAX*h, max(FACMIN*h, hnew) )
-  !       h = sign(one, xtarg-x) * min( MIDPT_GUARD*xmin, hnew )
-
-  !     else
-
-  !       ! -- NOT OK
-  !       nreject_ = nreject_ + 1
-  !       hnew = h*SAFE*err**(EXPERR)
-  !       h = sign(one, h) * max(FACMIN*h, min(hnew, h))
-  !       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-  !       ! ODE STALLS HERE
-  !       !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-
-  !     endif
-
-  !   enddo march
-
-  !   res = Y(1)
-
-  !   if(present(nsteps))  nsteps  = nsteps_
-  !   if(present(nreject)) nreject = nreject_
-
-  ! end function f21_ode_eval
 
 ! ================================================================================================================================ !
 end module rotex__hypergeometric
